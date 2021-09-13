46643527a6f14ad9b1fc66523d48cd551daecff4
Gustavo Gabas <gmgabas13@gmail.com>
2021-09-13 15:14:11 -0300

    refaturando para novo arquivo

--- src/server.js
+++ src/server.js
@@ -2,56 +2,17 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
   }
   
-  const axios = require('axios')
-  const fs = require('fs')
-  const path = require('path')
-  const https = require('https')
   const express = require('express')
-
-  const cert = fs.readFileSync(
-    path.resolve(__dirname, `../certs/${process.env.GN_CERT}`)
-  )
-  
-  const agent = new https.Agent({
-    pfx: cert,
-    passphrase: ''
-  })
-  
-  const credentials = Buffer.from(
-    `${process.env.GN_CLIENTE_ID}:${process.env.GN_CLIENTE_SECRET}`
-  ).toString('base64')
-  
+  const GNRequest = require('./api/gerencianet')
   const app = express()
 
   app.set('view engine', 'ejs')
   app.set('views', 'src/views')
 
-  app.get('/', async (req, res) => {
+  const reqGNAlready = GNRequest()
 
-    const authReponse = await axios({
-      method: 'POST',
-      url: `${process.env.GN_ENDPOINT}/oauth/token`,
-      headers: {
-        Authorization: `Basic ${credentials}`,
-        'Content-type': 'application/json'
-      },
-      httpsAgent: agent,
-      data: {
-        grant_type: 'client_credentials'
-      }
-    })
-    
-    const acessToken = authReponse.data?.access_token
-  
-    const reqGN = axios.create({
-       baseURL: process.env.GN_ENDPOINT,
-       httpsAgent: agent,
-       headers:{
-         Authorization: `Bearer ${acessToken}`,
-         'Content-type': 'application/json'
-       }
-     })
-  
+  app.get('/', async (req, res) => {
+    const reqGN = await reqGNAlready
      const dataCob = {
        calendario: {
          expiracao: 3600
