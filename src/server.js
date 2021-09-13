3ca26b884c2072eb0f223bf0b762a7e0b7787579
Gustavo Gabas <gmgabas13@gmail.com>
2021-09-13 14:19:23 -0300

    axios com config padrão

--- src/server.js
+++ src/server.js
@@ -20,6 +20,8 @@ if (process.env.NODE_ENV !== 'production') {
     `${process.env.GN_CLIENTE_ID}:${process.env.GN_CLIENTE_SECRET}`
   ).toString('base64')
   
+  
+  /*
   axios({
     method: 'POST',
     url: `${process.env.GN_ENDPOINT}/oauth/token`,
@@ -33,4 +35,43 @@ if (process.env.NODE_ENV !== 'production') {
     }
   }).then((response) => console.log(response.data))
   
+*/
+
+  
+  axios({
+    method: 'POST',
+    url: `${process.env.GN_ENDPOINT}/oauth/token`,
+    headers: {
+      Authorization: `Basic ${credentials}`,
+      'Content-type': 'application/json'
+    },
+    httpsAgent: agent,
+    data: {
+      grant_type: 'client_credentials'
+    }
+  }).then((response) => {
+    const acessToken = response.data?.access_token
+
+    const reqGN = axios.create({
+      baseURL: process.env.GN_ENDPOINT,
+      httpsAgent: agent,
+      headers:{
+        Authorization: `Bearer ${acessToken}`,
+        'Content-type': 'application/json'
+      }
+    })
+
+    const dataCob = {
+      calendario: {
+        expiracao: 3600
+      },
+      valor: {
+        original: '10.00'
+      },
+      chave: '5281d8850ae8dd100a64d5e7f45c07ff',
+      solicitacaoPagador: 'Cobrança dos serviços prestados.'
+    }
+
+    reqGN.post('/v2/cob', dataCob).then((response) => console.log(response.data))
+  })
   
